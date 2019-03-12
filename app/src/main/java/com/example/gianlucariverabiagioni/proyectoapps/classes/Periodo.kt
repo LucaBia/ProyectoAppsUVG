80dbf6bfaf510361a271427de27f9717c78c813c
Francisco Rosal <francisco-rosal98@hotmail.com>
2019-03-11 22:20:06 -0600

    Avances

---
+++ app/src/main/java/com/example/gianlucariverabiagioni/proyectoapps/classes/Periodo.kt
@@ -0,0 +1,11 @@
+package com.example.gianlucariverabiagioni.proyectoapps.classes
+
+class Periodo {
+    val inicioHora : String
+    val finalHora : String
+
+    constructor(inicioH: String, finalH: String) {
+        this.inicioHora = inicioH
+        this.finalHora = finalH
+    }
+}
