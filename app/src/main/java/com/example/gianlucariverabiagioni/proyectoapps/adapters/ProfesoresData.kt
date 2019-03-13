eb10b7b6f62f805066fd5ccf217df3731f8f5485
Francisco Rosal <francisco-rosal98@hotmail.com>
2019-03-13 01:07:01 -0600

    Avances

--- app/src/main/java/com/example/gianlucariverabiagioni/proyectoapps/adapters/ProfesoresData.kt
+++ app/src/main/java/com/example/gianlucariverabiagioni/proyectoapps/adapters/ProfesoresData.kt
@@ -1,71 +1,21 @@
 package com.example.gianlucariverabiagioni.proyectoapps.adapters
 
-import com.example.gianlucariverabiagioni.proyectoapps.R
-
 object ProfesoresData {
 
     internal var nameArray = arrayOf(
-        "Cupcake1",
-        "Donut2",
-        "Eclair3",
-        "Froyo4",
-        "Gingerbread5",
-        "Honeycomb6",
-        "Ice Cream Sandwich7",
-        "JellyBean8",
-        "Kitkat9",
-        "Marshmallow10"
-    )
-    internal var carneArray = arrayOf(
-        "123451",
-        "123452",
-        "123453",
-        "123454",
-        "123455",
-        "123456",
-        "123457",
-        "123458",
-        "123459",
-        "123410"
+        "Pedro Luis Alfonso Lopez",
+        "Diego Alejandro Enriquez Rodriguez",
+        "Magda Fabiola Moscoso Arriola",
+        "Zayda Rita Pérez Zubillaga",
+        "Martha Ligia Naranjo Franky",
+        "Oscar Estuardo Gil Sanchez"
     )
     internal var correoArray = arrayOf(
-        "correo1@uvg.edu.gt",
-        "correo2@uvg.edu.gt",
-        "correo3@uvg.edu.gt",
-        "correo4@uvg.edu.gt",
-        "correo5@uvg.edu.gt",
-        "correo6@uvg.edu.gt",
-        "correo7@uvg.edu.gt",
-        "correo8@uvg.edu.gt",
-        "correo9@uvg.edu.gt",
-        "correo10@uvg.edu.gt"
+        "palf@uvg.edu.gt",
+        "dalef@uvg.edu.gt",
+        "mmos@uvg.edu.gt",
+        "zper@uvg.edu.gt",
+        "mnar@uvg.edu.gt",
+        "ogil@uvg.edu.gt"
     )
-
-    internal var contrasenaArray = arrayOf(
-        "abcdef1",
-        "abcdef2",
-        "abcdef3",
-        "abcdef4",
-        "abcdef5",
-        "abcdef6",
-        "abcdef7",
-        "abcdef8",
-        "abcdef9",
-        "abcdef10"
-    )
-
-    internal var drawableArray = arrayOf<Int>(
-        R.drawable.ic_launcher,
-        R.drawable.ic_launcher,
-        R.drawable.ic_launcher,
-        R.drawable.ic_launcher,
-        R.drawable.ic_launcher,
-        R.drawable.ic_launcher,
-        R.drawable.ic_launcher,
-        R.drawable.cit1,
-        R.drawable.ic_launcher,
-        R.drawable.ic_launcher
-    )
-
-    internal var id_ = arrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
 }
