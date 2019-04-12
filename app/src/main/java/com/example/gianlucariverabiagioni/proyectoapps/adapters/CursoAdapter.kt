ec5b48aab0bfe7a87c704859c3199a09c389feb1
Francisco Rosal <francisco-rosal98@hotmail.com>
2019-04-11 19:33:56 -0600

    Ya casi horario

--- app/src/main/java/com/example/gianlucariverabiagioni/proyectoapps/adapters/CursoAdapter.kt
+++ app/src/main/java/com/example/gianlucariverabiagioni/proyectoapps/adapters/CursoAdapter.kt
@@ -8,28 +8,42 @@ import android.widget.BaseAdapter
 import android.widget.TextView
 import com.example.gianlucariverabiagioni.proyectoapps.R
 import com.example.gianlucariverabiagioni.proyectoapps.classes.Horario
+import android.support.v4.content.ContextCompat.getSystemService
 
-class CursoAdapter : BaseAdapter {
+
+
+class CursoAdapter(context: Context, dataHorario: Horario) : BaseAdapter() {
 
     private var context: Context
     private var dataHorario: ArrayList<String?>
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        if (convertView == null) {
-            val convertView1 = LayoutInflater.from(parent!!.context).inflate(R.layout.curso_item, parent, false)
-            return convertView1
-        }
+    /*constructor(context: Context, dataHorario: Horario) {
+        //this.context = context
+        this.dataHorario = dataHorario.toArray()
+    }*/
 
-        var textViewName: TextView = convertView.findViewById(R.id.text_view_nombre) as TextView
-        var textViewSalon: TextView = convertView.findViewById(R.id.text_view_salon) as TextView
-        textViewName.text = dataHorario[position]
-
-        return convertView
-    }
-
-    constructor(context: Context, dataHorario: Horario) {
+    init {
         this.context = context
         this.dataHorario = dataHorario.toArray()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var convertView1: View
+
+        if (convertView == null) {
+            val layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            convertView1 = layoutInflater.inflate(R.layout.curso_item, null, false)
+            //val convertView1 = LayoutInflater.from(parent!!.context).inflate(R.layout.curso_item, parent, false)
+            //return convertView1
+        } else {
+            convertView1 = convertView
+        }
+
+        var textViewName: TextView = convertView1.findViewById(R.id.text_view_nombre) as TextView
+        var textViewSalon: TextView = convertView1.findViewById(R.id.text_view_salon) as TextView
+        textViewName.text = dataHorario[position]
+
+        return convertView1
     }
 
     override fun getItem(position: Int): Any? {
